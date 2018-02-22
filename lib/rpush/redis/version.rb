27900b0227b7d268821f11cb55d421f2b342e395
Anton Rieder <aried3r@gmail.com>
2018-02-22 15:16:53 +0100

    Prepare v1.0.1 release
    I messed up the 1.0.0 release so I had to yank the gem and republish.

--- lib/rpush/redis/version.rb
+++ lib/rpush/redis/version.rb
@@ -3,7 +3,7 @@ module Rpush
     module VERSION
       MAJOR = 1
       MINOR = 0
-      TINY = 0
+      TINY = 1
       PRE = nil
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".").freeze
