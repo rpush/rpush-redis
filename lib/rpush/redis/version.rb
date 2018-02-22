57626a9dd9afea20b9b0e87a937c955235b77ce5
Anton Rieder <aried3r@gmail.com>
2018-02-22 15:11:55 +0100

    Prepare 1.0.0 release

--- lib/rpush/redis/version.rb
+++ lib/rpush/redis/version.rb
@@ -1,5 +1,16 @@
 module Rpush
   module Redis
-    VERSION = '0.4.1'
+    module VERSION
+      MAJOR = 1
+      MINOR = 0
+      TINY = 0
+      PRE = nil
+
+      STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".").freeze
+
+      def self.to_s
+        STRING
+      end
+    end
   end
 end
